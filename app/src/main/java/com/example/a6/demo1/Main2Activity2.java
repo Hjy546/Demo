32a9e30fb46eca5f9eb8842e29020da27bde8e5a
zzzz <1556874368@qq.com>
2020-10-23 13:09:48 +0800

    6

--- app/src/main/java/com/example/a6/demo1/Main2Activity2.java
+++ app/src/main/java/com/example/a6/demo1/Main2Activity2.java
@@ -2,6 +2,7 @@ package com.example.a6.demo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main2Activity2 extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Main2Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
+        Log.d(">>>", "b");
     }
 }
