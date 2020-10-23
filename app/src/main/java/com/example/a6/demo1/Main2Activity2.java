87ca6ab065dcf2e52337d09c961198e1ab11d685
zzzz <1556874368@qq.com>
2020-10-23 13:13:28 +0800

    5

--- app/src/main/java/com/example/a6/demo1/Main2Activity2.java
+++ app/src/main/java/com/example/a6/demo1/Main2Activity2.java
@@ -11,6 +11,6 @@ public class Main2Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main22);
         Log.d(">>>", "b");
-        Log.d(">>>", "a");
+        Log.d(">>>", "c");
     }
 }
