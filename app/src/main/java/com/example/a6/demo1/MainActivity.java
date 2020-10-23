6e6f541f8e3c5f195781572b70b2c436e87f064b
zzzz <1556874368@qq.com>
2020-10-23 12:59:04 +0800

    2

--- app/src/main/java/com/example/a6/demo1/MainActivity.java
+++ app/src/main/java/com/example/a6/demo1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.a6.demo1;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                startActivity(new Intent(this, Main2Activity2.class));
+                break;
+            default:
+                break;
+        }
     }
 }
